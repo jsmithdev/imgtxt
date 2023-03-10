3ccc6707b8ee4d39f369f47066d9d03f5f403b42
Jamie Smith <me@jsmith.dev>
2023-03-10 15:02:38 -0500

    pass in import.meta for meow

--- cli.js
+++ cli.js
@@ -15,8 +15,10 @@ const cli = meow(`
   Notes
     path can be a local path to an image or a url to an image
 	  local path can be encoded or not, for example 'my img.png' or 'my%20img.png' works
-
-`);
+	`, {
+		importMeta: import.meta,
+		flags: {},
+	});
 
 const input = cli.input.join(' ');
 
